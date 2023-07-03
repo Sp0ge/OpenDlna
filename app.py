61af0e27df91c94237a5479240e16f9175bf15e6
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2023-07-03 15:44:39 +0300

    dev

--- app.py
+++ app.py
@@ -1,56 +1,23 @@
-"""Example of streaming a file and printing status updates.
-
-python stream.py 10.0.0.4 file.mp3
-"""
-
-import asyncio
 import sys
+from opendlna.GUI import app as flask_web_gui
 
-import pyatv
-from pyatv.interface import Playing, PushListener
+web_interface_active = True
 
-LOOP = asyncio.get_event_loop()
+for arg_n in range(1,len(sys.argv),2):
+    arg = sys.argv[arg_n]
+    param = sys.argv[arg_n+1]
+    if arg == "-h" or arg == "--help":
+        params = [
+            '-h, --help \t  info about commands',
+            '-wg, --web \t  web gui (True/False) default=true'
+        ]
+        
+        for param in params:
+            print(param)
+            
+    if arg == "-wg" or arg == "--web":
+        web_interface_active = str(param)
+        
 
-
-class PushUpdatePrinter(PushListener):
-    """Print push updates to console."""
-
-    def playstatus_update(self, updater, playstatus: Playing) -> None:
-        """Inform about changes to what is currently playing."""
-        print(30 * "-" + "\n", playstatus)
-
-    def playstatus_error(self, updater, exception: Exception) -> None:
-        """Inform about an error when updating play status."""
-        print("Error:", exception)
-
-
-async def stream_with_push_updates(
-    address: str, filename: str, loop: asyncio.AbstractEventLoop
-):
-    """Find a device and print what is playing."""
-    print("* Discovering device on network...")
-    atvs = await pyatv.scan(loop, hosts=[address], timeout=5)
-
-    if not atvs:
-        print("* Device found", file=sys.stderr)
-        return
-
-    conf = atvs[0]
-
-    print("* Connecting to", conf.address)
-    atv = await pyatv.connect(conf, loop)
-
-    listener = PushUpdatePrinter()
-    atv.push_updater.listener = listener
-    atv.push_updater.start()
-
-    try:
-        print("* Starting to stream", filename)
-        await atv.stream.stream_file(filename)
-        await asyncio.sleep(1)
-    finally:
-        atv.close()
-
-
-if __name__ == "__main__":
-    LOOP.run_until_complete(stream_with_push_updates(sys.argv[1], sys.argv[2], LOOP))
+if web_interface_active:
+    flask_web_gui.run(host="localhost", port=80)
